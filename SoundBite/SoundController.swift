b235d00508ed447c6ee4ed1cac21a4cc5c400f72
Logan Geefs <lgeefs@gmail.com>
2017-03-11 16:12:28 -0800

    .

---
+++ SoundBite/SoundController.swift
@@ -0,0 +1,305 @@
+//
+//  SoundController.swift
+//  SoundBite
+//
+//  Created by Logan Geefs on 2017-03-11.
+//  Copyright © 2017 LoganGeefs. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+protocol SoundControllerDelegate {
+    
+    
+    
+}
+
+struct DirectoryNames {
+    
+    static let rawfiles = "rawfiles"
+    static let soundbits = "soundbits"
+    static let finishedFiles = "soundbites"
+    
+}
+
+class SoundController: NSObject, AVAudioRecorderDelegate, AVAudioPlayerDelegate {
+    
+    var delegate: SoundControllerDelegate?
+    
+    let session = AVAudioSession.sharedInstance()
+    
+    var recorder: AVAudioRecorder!
+    var player: AVAudioPlayer!
+    
+    var timer: Timer!
+    
+    override init() {
+        super.init()
+        
+        self.createDirectory(DirectoryNames.rawfiles)
+        self.createDirectory(DirectoryNames.soundbits)
+        self.createDirectory(DirectoryNames.finishedFiles)
+        
+        setupRecorder()
+        print(getFileCount())
+        
+        timer = Timer.scheduledTimer(timeInterval: 10, target: self, selector: #selector(timerFired), userInfo: nil, repeats: true)
+        
+        self.startRecording()
+        
+    }
+    
+    func timerFired(timer: Timer) {
+        
+        self.stopRecording()
+        
+        self.startRecording()
+        
+    }
+    
+    func saveSoundbite() {
+        
+        self.stopRecording()
+        
+        let audioURL = self.recorder.url
+        
+        if let asset = AVAsset.init(url: url) as? AVAsset {
+            let name = "\(getFileCount(DirectoryNames.soundbits)).m4a"
+            exportAsset(asset, name)
+        }
+        
+        self.startRecording()
+        
+    }
+    
+    func exportAsset(_ asset: AVAsset, _ name: String) {
+        
+        let fileManager = FileManager.default
+        
+        let documentsDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let directoryPath = documentsDirectory.appendingPathComponent(DirectoryNames.soundbits)
+        let trimmedFileURL = directoryPath.appendingPathComponent(name)
+        
+        print("Saving to \(trimmedFileURL.absoluteString)")
+        
+        if fileManager.fileExists(atPath: trimmedFileURL.absoluteString) {
+            print("File exists")
+        }
+        
+        let exporter = AVAssetExportSession(asset: asset, presetName: AVAssetExportPresetAppleM4A)
+        exporter?.outputFileType = AVFileTypeAppleM4A
+        exporter?.outputURL = trimmedFileURL
+        
+        let soundbitsFileCount = getFileCount(DirectoryNames.soundbits)
+        
+        if soundbitsFileCount < 1 {
+            print("Error: soundbits file count is less than 1")
+            return
+        }
+        
+        let duration = CMTimeGetSeconds(asset.duration)
+        
+        let previousSoundbitFileStartTime = CMTimeMake(<#T##value: Int64##Int64#>, <#T##timescale: Int32##Int32#>)
+        
+    }
+    
+    func getFileCount(_ directoryName: String) -> Int {
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let directoryPath = documentsDirectory.appendingPathComponent(name)
+        let dirContents = try? fileManager.contentsOfDirectory(atPath: directoryPath)
+        let count = dirContents?.count
+        return count ?? 0
+    }
+    
+    func createDirectory(_ name: String) {
+    
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let dataPath = documentsDirectory.appendingPathComponent(name)
+        
+        if FileManager.default.fileExists(atPath: dataPath.path) {
+            return
+        }
+        
+        do {
+            try FileManager.default.createDirectory(atPath: dataPath.path, withIntermediateDirectories: true, attributes: nil)
+        } catch let error as NSError {
+            print("Error creating directory: \(error.localizedDescription)")
+        }
+        
+    }
+    
+    func setupRecorder() {
+        
+        let format = DateFormatter()
+        format.dateFormat="yyyy-MM-dd-HH:mm:ss"
+        //let currentFileName = "recording-\(format.string(from: Date())).m4a"
+        let currentFileName = "recording\(getFileCount(DirectoryNames.rawfiles)).m4a"
+        print("Current filename: \(currentFileName)")
+        print()
+        
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let rawFileDirectory = documentsDirectory.appendingPathComponent(DirectoryNames.rawfiles)
+        let soundFileURL = rawFileDirectory.appendingPathComponent(currentFileName)
+        print("Writing to url: '\(soundFileURL)'")
+        print()
+        
+        /*if FileManager.default.fileExists(atPath: soundFileURL.absoluteString) {
+            // probably won't happen. want to do something about it?
+            print("File \(soundFileURL.absoluteString) exists")
+        }*/
+        
+        let recordSettings:[String : AnyObject] = [
+            AVFormatIDKey:             NSNumber(value: kAudioFormatAppleLossless),
+            AVEncoderAudioQualityKey : NSNumber(value:AVAudioQuality.max.rawValue),
+            AVEncoderBitRateKey :      NSNumber(value:320000),
+            AVNumberOfChannelsKey:     NSNumber(value:2),
+            AVSampleRateKey :          NSNumber(value:44100.0)
+        ]
+        
+        do {
+            recorder = try AVAudioRecorder(url: soundFileURL, settings: recordSettings)
+            recorder.delegate = self
+            recorder.prepareToRecord()
+            recorder.record()
+        } catch let error {
+            recorder = nil
+            print(error.localizedDescription)
+        }
+        
+    }
+    
+    func startRecording() {
+        
+        if player != nil && player.isPlaying {
+            player.stop()
+        }
+        
+        if recorder == nil {
+            print("recorder nil")
+            /*recorder.setTitle("Pause", for: .normal)
+            playButton.isEnabled = false
+            stopButton.isEnabled = true
+            recordWithPermission(true)*/
+            return
+        }
+        
+        if recorder != nil && recorder.isRecording {
+            print("pausing")
+            recorder.pause()
+            //recordButton.setTitle("Continue", for: .normal)
+            
+        } else {
+            print("recording")
+            /*recordButton.setTitle("Pause", for:UIControlState())
+            playButton.isEnabled = false
+            stopButton.isEnabled = true
+            recorder.record()*/
+            recordWithPermission(false)
+        }
+        
+    }
+    
+    func recordWithPermission(_ setup:Bool) {
+
+        if (session.responds(to: #selector(AVAudioSession.requestRecordPermission(_:)))) {
+            AVAudioSession.sharedInstance().requestRecordPermission({(granted: Bool)-> Void in
+                if granted {
+                    print("Permission to record granted")
+                    self.setSessionPlayAndRecord()
+                    if setup {
+                        self.setupRecorder()
+                    }
+                    self.recorder.record()
+                    print("WORKS")
+                    
+                } else {
+                    print("Permission to record not granted")
+                }
+            })
+        } else {
+            print("requestRecordPermission unrecognized")
+        }
+        
+    }
+    
+    func setSessionPlayAndRecord() {
+        
+        do {
+            try session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+        } catch let error as NSError {
+            print("could not set session category")
+            print(error.localizedDescription)
+        }
+        do {
+            try session.setActive(true)
+        } catch let error as NSError {
+            print("could not make session active")
+            print(error.localizedDescription)
+        }
+        
+    }
+    
+    func stopRecording() {
+        
+        self.recorder.stop()
+        print(self.recorder.url)
+        
+    }
+    
+    func deleteAllRecordings() {
+        let docsDir =
+            NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+        
+        let fileManager = FileManager.default
+        
+        do {
+            let files = try fileManager.contentsOfDirectory(atPath: docsDir)
+            var recordings = files.filter( { (name: String) -> Bool in
+                return (name.hasPrefix("recording") && name.hasSuffix("m4a"))
+            })
+            for i in 0 ..< recordings.count {
+                let path = docsDir + "/" + recordings[i]
+                
+                print("removing \(path)")
+                do {
+                    try fileManager.removeItem(atPath: path)
+                } catch let error as NSError {
+                    NSLog("could not remove \(path)")
+                    print(error.localizedDescription)
+                }
+            }
+            
+        } catch let error as NSError {
+            print("could not get contents of directory at \(docsDir)")
+            print(error.localizedDescription)
+        }
+        
+    }
+    
+    func startPlaying() {
+        
+        var url:URL?
+        if self.recorder != nil {
+            url = self.recorder.url
+        } else {
+            return
+        }
+        
+        print("playing \(url)")
+        
+        do {
+            self.player = try AVAudioPlayer(contentsOf: url!)
+            player.delegate = self
+            player.prepareToPlay()
+            player.volume = 1.0
+            player.play()
+        } catch let error as NSError {
+            self.player = nil
+            print(error.localizedDescription)
+        }
+        
+    }
+        
+    
+}
--- SoundBite/ViewController.swift
+++ SoundBite/ViewController.swift
@@ -9,10 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var finishButton: UIButton!
+    
+    var soundController: SoundController!
+    
+    var startTimer: Timer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        startTimer = Timer.scheduledTimer(timeInterval: 10, target: self, selector: #selector(activateSoundbiteButton(timer:)), userInfo: nil, repeats: false)
+
+        recordButton.isEnabled = false
+        recordButton.addTarget(self, action: #selector(recordButtonPressed(sender:)), for: .touchUpInside)
+        playButton.addTarget(self, action: #selector(playButtonPressed(sender:)), for: .touchUpInside)
+        finishButton.addTarget(self, action: #selector(finishButtonPressed(sender:)), for: .touchUpInside)
+        
+        soundController = SoundController()
+        soundController.startRecording()
+    
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +37,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func recordButtonPressed(sender: UIButton) {
+        
+        
+        
+    }
+    
+    func activateSoundbiteButton(timer: Timer) {
+        
+        recordButton.isEnabled = true
+        
+    }
+    
+    func playButtonPressed(sender: UIButton) {
+        soundController.startPlaying()
+    }
+    
+    func finishButtonPressed(sender: UIButton) {
+        
+        soundController.stopRecording()
+        soundController.timer.invalidate()
+        
+    }
 
 }
 
